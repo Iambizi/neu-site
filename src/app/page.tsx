4c6917e6af7cbe678ce236b7ea4315d177297851
Iambizi <amirbzmn@gmail.com>
2023-08-01 00:38:36 -0400

    converts glb to jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,28 @@ import { Canvas } from '@react-three/fiber';
 import { useGLTF, OrbitControls, Environment, useTexture } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import * as THREE from 'three';
+import  WIP  from './components/WIP';
 import styles from './page.module.css'
+
 
 type ModelProps = {
   setHovered: (hovered: boolean) => void;
 };
 
-const Model: FC<ModelProps> = ({ setHovered }) => {
-  const gltf = useGLTF('/WIP.glb', false);
-  return (
-    <primitive
-      object={gltf.scene}
-      position={[0, 0, 0]}
-      scale={0.25}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      onTouchStart={() => setHovered(true)}
-      onTouchEnd={() => setHovered(false)}
-    />
-  );
-};
+// const Model: FC<ModelProps> = ({ setHovered }) => {
+//   const gltf = useGLTF('/WIP.glb', false);
+//   return (
+//     <primitive
+//       object={gltf.scene}
+//       position={[0, 0, 0]}
+//       scale={0.25}
+//       onPointerOver={() => setHovered(true)}
+//       onPointerOut={() => setHovered(false)}
+//       onTouchStart={() => setHovered(true)}
+//       onTouchEnd={() => setHovered(false)}
+//     />
+//   );
+// };
 
 type MyEnvironmentProps = {
   path: string;
@@ -63,16 +65,19 @@ const App: FC = () => {
       <Canvas style={{ display: 'block', margin: '0 auto' }} camera={{ position: [0, 0, 2], fov: 40 }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Model setHovered={setHovered} />
-        <OrbitControls autoRotate autoRotateSpeed={isHovered ? 0.15 : 1} minDistance={1} maxDistance={5} /> {/* Change speed based on isHovered state */}
+        {/* <Model setHovered={setHovered} /> */}
+        <WIP setHovered={setHovered} />
+        <OrbitControls autoRotate autoRotateSpeed={isHovered ? 0.15 : 1} minDistance={1} maxDistance={5} target={[0, 0, 0]} />
         <Environment preset="dawn" background />
         {/* <MyEnvironment path="/HDRI/AdobeStock_Galaxy.jpeg" /> */}
       </Canvas>
       <div className="App-logo-container">
         <img src="./assets/3D_Asset.png" alt="Logo" className="App-logo-img" />
+        {/* <img src={logo} alt="Logo" /> */}
       </div>
     </div>
   );
 };
 
 export default App;
+
